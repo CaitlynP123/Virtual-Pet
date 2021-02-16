ef631f2a3b69e7dabb60bc586148f3e28d3a1d6d
CaitlynP123 <74350761+CaitlynP123@users.noreply.github.com>
2021-02-16 14:31:06 +0530

    Update Food.js

--- Js/Food.js
+++ Js/Food.js
@@ -1,7 +1,7 @@
 class Food {
     constructor(){
         this.foodStock = 20
-        this.image = loadImage('images/Milk.png');
+        this.image = loadImage("Images/Milk.png");
     }
 
    updateFoodStock(foodStock){
